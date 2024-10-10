858ff46d1991cde53abac7885e783ad5b7f161d7
Aux <justin@akitaux.com>
2024-10-09 20:34:03 -0500

    First draft faq command

---
+++ src/Valiant.Core/Models/Faq/FaqEntry.cs
@@ -0,0 +1,10 @@
+﻿using LiteDB;
+
+namespace Valiant.Models;
+
+public class FaqEntry
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string ThreadUrl { get; set; }
+}
