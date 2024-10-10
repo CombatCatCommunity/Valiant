b5dc724385946e8389721d9ec62e1adb2a819d7e
Aux <justin@akitaux.com>
2024-10-09 20:34:39 -0500

    Add interaction handler

---
+++ src/Valiant/Services/DiscordStartupService.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Valiant.Utility;
+
+namespace Valiant.Services;
+
+public class DiscordStartupService : IHostedService
+{
+    private readonly DiscordSocketClient _discord;
+    private readonly IConfiguration _config;
+    private readonly ILogger _logger;
+
+    public DiscordStartupService(
+        DiscordSocketClient discord,
+        IConfiguration config,
+        ILogger<DiscordStartupService> logger)
+    {
+        _discord = discord;
+        _config = config;
+        _logger = logger;
+
+        _discord.Log += msg => LogHelper.OnLogAsync(_logger, msg);
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await _discord.LoginAsync(TokenType.Bot, _config["discord:token"]);
+        await _discord.StartAsync();
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await _discord.LogoutAsync();
+        await _discord.StopAsync();
+    }
+}
---
+++ src/Valiant/Services/InteractionHandlingService.cs
@@ -0,0 +1,64 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+using Valiant.Utility;
+
+namespace Valiant.Services;
+
+public class InteractionHandlingService : IHostedService
+{
+    private readonly DiscordSocketClient _discord;
+    private readonly InteractionService _interactions;
+    private readonly IServiceProvider _services;
+    private readonly ILogger _logger;
+
+    public InteractionHandlingService(
+        DiscordSocketClient discord,
+        InteractionService interactions,
+        IServiceProvider services,
+        ILogger<InteractionHandlingService> logger)
+    {
+        _discord = discord;
+        _interactions = interactions;
+        _services = services;
+        _logger = logger;
+
+        _interactions.Log += msg => LogHelper.OnLogAsync(_logger, msg);
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        _discord.Ready += () => _interactions.RegisterCommandsGloballyAsync(true);
+        _discord.InteractionCreated += OnInteractionAsync;
+
+        await _interactions.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _interactions.Dispose();
+        return Task.CompletedTask;
+    }
+
+    private async Task OnInteractionAsync(SocketInteraction interaction)
+    {
+        try
+        {
+            var context = new SocketInteractionContext(_discord, interaction);
+            var result = await _interactions.ExecuteCommandAsync(context, _services);
+
+            if (!result.IsSuccess)
+                await interaction.RespondAsync(result.ToString(), ephemeral: true);
+        } catch
+        {
+            if (interaction.Type == InteractionType.ApplicationCommand)
+            {
+                await interaction.GetOriginalResponseAsync()
+                    .ContinueWith(msg => msg.Result.DeleteAsync());
+            }
+        }
+    }
+}
