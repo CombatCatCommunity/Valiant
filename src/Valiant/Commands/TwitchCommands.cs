ba5ba8a591ef4993a805057657aff984df2fae7b
Aux <justin@akitaux.com>
2024-10-09 20:34:21 -0500

    Add text command handling

---
+++ src/Valiant/Commands/TwitchCommands.cs
@@ -0,0 +1,43 @@
+﻿using Discord;
+using Discord.Commands;
+using LiteDB;
+using Valiant.Models;
+
+namespace Valiant.Commands;
+
+public class TwitchCommands : ModuleBase<SocketCommandContext>
+{
+    [Command("forcetwitchstats")]
+    [RequireUserPermission(GuildPermission.Administrator)]
+    public async Task ForceTwitchStatsAsync()
+    {
+        using var db = new LiteDatabase("Filename=./data/twitch.db;ReadOnly=true");
+        var stats = db.GetCollection<TwitchStats>().Query()
+            .Where(x => x.Timestamp > DateTime.UtcNow.AddDays(-1)).ToList();
+
+        var orderedStreams = stats.OrderByDescending(x => x.StreamerCount);
+        var maxStreams = orderedStreams.FirstOrDefault();
+        var minStreams = orderedStreams.LastOrDefault();
+        double avgStreams = stats.DefaultIfEmpty().Average(x => x?.StreamerCount ?? 0);
+
+        var orderedViews = stats.OrderByDescending(x => x.TotalViewers);
+        var maxViewers = orderedViews.FirstOrDefault();
+        var minViewers = orderedViews.LastOrDefault();
+        double avgViewers = stats.DefaultIfEmpty().Average(x => x?.TotalViewers ?? 0);
+
+        var popularity = stats.OrderByDescending(x => x.MostPopularChannel.ViewerCount).FirstOrDefault();
+
+        var channel = Context.Client.GetGuild(1209429896317771796).GetTextChannel(1293324440339484743);
+        await channel.SendMessageAsync($"## ShawnStats for {DateTime.Today.AddDays(-1):dddd, MMMM dd}\n" +
+            $"**Streams**\n" +
+            $"**Max**: {maxStreams?.StreamerCount ?? 0} at <t:{new DateTimeOffset(maxStreams?.Timestamp ?? DateTime.MinValue).ToUnixTimeSeconds()}:t>\n" +
+            $"**Min**: {minStreams?.StreamerCount ?? 0} at <t:{new DateTimeOffset(minStreams?.Timestamp ?? DateTime.MinValue).ToUnixTimeSeconds()}:t>\n" +
+            $"**Avg**: {Math.Round(avgStreams, 1)}\n" +
+            $"**Viewers**\n" +
+            $"**Max**: {maxViewers?.TotalViewers ?? 0} at <t:{new DateTimeOffset(maxViewers?.Timestamp ?? DateTime.MinValue).ToUnixTimeSeconds()}:t>\n" +
+            $"**Min**: {minViewers?.TotalViewers ?? 0} at <t:{new DateTimeOffset(minViewers?.Timestamp ?? DateTime.MinValue).ToUnixTimeSeconds()}:t>\n" +
+            $"**Avg**: {Math.Round(avgViewers, 1)}\n" +
+            $"**Most Popular Stream:** <https://twitch.tv/{popularity?.MostPopularChannel.Name}> " +
+                $"at <t:{new DateTimeOffset(popularity?.Timestamp ?? DateTime.MinValue).ToUnixTimeSeconds()}:t>");
+    }
+}
---
+++ src/Valiant/Services/CommandHandlingService.cs
@@ -0,0 +1,66 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+using Valiant.Commands;
+using Valiant.Utility;
+
+namespace Valiant.Services;
+
+public class CommandHandlingService : IHostedService
+{
+    private readonly DiscordSocketClient _discord;
+    private readonly CommandService _commands;
+    private readonly IServiceProvider _services;
+    private readonly ILogger _logger;
+
+    public CommandHandlingService(
+        DiscordSocketClient discord,
+        CommandService commands,
+        IServiceProvider services,
+        ILogger<CommandHandlingService> logger)
+    {
+        _discord = discord;
+        _commands = commands;
+        _services = services;
+        _logger = logger;
+
+        _commands.Log += msg => LogHelper.OnLogAsync(_logger, msg);
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        _discord.MessageReceived += OnMessageReceivedAsync;
+
+        await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        _logger.LogInformation($"Loaded {_commands.Commands.Count()} command(s)");
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _discord.MessageReceived -= OnMessageReceivedAsync;
+        return Task.CompletedTask;
+    }
+
+    private async Task OnMessageReceivedAsync(SocketMessage message)
+    {
+        if (message is not SocketUserMessage msg)
+            return;
+        int argPos = 0;
+        if (!msg.HasStringPrefix("vally!", ref argPos))
+            return;
+
+        try
+        {
+            var context = new SocketCommandContext(_discord, msg);
+            var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+            if (!result.IsSuccess)
+                return;
+        } catch
+        {
+
+        }
+    }
+}
