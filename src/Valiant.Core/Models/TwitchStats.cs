d82bd58d723125f9fcf5d3b385c3c4a4e04cf067
Aux <justin@akitaux.com>
2024-10-09 20:34:32 -0500

    Add twitch watcher service

---
+++ src/Valiant.Core/Models/TwitchStats.cs
@@ -0,0 +1,11 @@
+﻿namespace Valiant.Models;
+
+public class TwitchStats
+{
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    public int StreamerCount { get; set; }
+    public int TotalViewers { get; set; }
+    public PopularChannel MostPopularChannel { get; set; }
+}
+
+public record class PopularChannel(string Name, int ViewerCount);
