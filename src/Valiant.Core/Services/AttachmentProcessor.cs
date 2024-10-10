f93675985db69be345e4d02993af61fac748ebfe
Aux <justin@akitaux.com>
2024-10-09 20:33:47 -0500

    First draft OCR service

---
+++ src/Valiant.Core/Services/AttachmentProcessor.cs
@@ -0,0 +1,100 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using Tesseract;
+
+namespace Valiant.Services;
+
+public class AttachmentProcessor : IHostedService
+{
+    private readonly ILogger<AttachmentProcessor> _logger;
+    private readonly DiscordSocketClient _discord;
+    private readonly HttpClient _http;
+
+    private TesseractEngine _tesseract;
+    private SocketTextChannel _logTo;
+
+    public AttachmentProcessor(ILogger<AttachmentProcessor> logger, DiscordSocketClient discord, HttpClient http)
+    {
+        _logger = logger;
+        _discord = discord;
+        _http = http;
+
+        _discord.Ready += OnReadyAsync;
+    }
+
+    private Task OnReadyAsync()
+    {
+        _logTo = _discord.GetGuild(1209429896317771796).GetTextChannel(1292332722185830400);
+        return Task.CompletedTask;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        if (!Directory.Exists(Constants.TessdataDirectory))
+        {
+            _logger.LogError("Tesseract training data not found, not starting service.");
+            return Task.CompletedTask;
+        }
+
+        _discord.MessageReceived += OnMessageReceivedAsync;
+        _tesseract = new(Constants.TessdataDirectory, "eng");
+
+        _logger.LogInformation("Started");
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _discord.MessageReceived -= OnMessageReceivedAsync;
+        _tesseract?.Dispose();
+        _tesseract = null;
+
+        _logger.LogInformation("Stopped");
+        return Task.CompletedTask;
+    }
+
+    private async Task OnMessageReceivedAsync(SocketMessage message)
+    {
+        if (_tesseract == null)
+        {
+            _logger.LogError("Message handler active but tesseract is null");
+            return;
+        }
+
+        var targetImages = message.Attachments.Where(x => x.ContentType.StartsWith("image"));
+        if (!targetImages.Any())
+            return;
+
+        var results = new List<Embed>();
+        foreach (var attachment in targetImages)
+        {
+            var timer = Stopwatch.StartNew();
+            var bits = await _http.GetByteArrayAsync(attachment.Url);
+            var image = Pix.LoadFromMemory(bits);
+
+            using var page = _tesseract.Process(image);
+            
+            var resultText = page.GetText();
+            if (string.IsNullOrWhiteSpace(resultText))
+            {
+                _logger.LogInformation($"Skipping an attachment in msg with id {message.Id}, no text found");
+                continue;
+            }
+
+            var embed = new EmbedBuilder()
+                .WithTitle($"OCR Result in {timer.ElapsedMilliseconds}ms with {page.GetMeanConfidence() * 100}% confidence")
+                .WithDescription(Format.Code(resultText))
+                .WithImageUrl(attachment.Url);
+            results.Add(embed.Build());
+
+            timer.Stop();
+            _logger.LogInformation($"Processed an image for msg with id {message.Id} in {timer.ElapsedMilliseconds}ms");
+        }
+
+        foreach (var embed in results)
+            await _logTo.SendMessageAsync(message.GetJumpUrl(), embed: embed);
+    }
+}
---
+++ src/Valiant/Interactions/OCRModule.cs
@@ -0,0 +1,88 @@
+﻿using Discord;
+using Discord.Interactions;
+using System.Diagnostics;
+using System.Drawing;
+using Tesseract;
+
+namespace Valiant.Interactions;
+
+public class OCRModule : InteractionModuleBase<SocketInteractionContext>
+{
+    private readonly HttpClient _http;
+
+    public OCRModule(HttpClient http)
+    {
+        _http = http;
+    }
+
+    [RequireUserPermission(GuildPermission.ModerateMembers)]
+    [SlashCommand("ocr", "Force run OCR for a specific message")]
+    public async Task OCRAsync([Summary(description: "A link to the message with attachments to read")]string messageUrl, int index = 0)
+    {
+        var timer = Stopwatch.StartNew();
+        if (!Uri.TryCreate(messageUrl, default, out _))
+        {
+            await RespondAsync($"The provided string `{messageUrl}` is not a valid url");
+            return;
+        }
+
+        var noDomain = messageUrl.Replace("https://discord.com/channels/", "")
+                                 .Replace("https://canary.discord.com/channels/", "");
+        var ids = noDomain.Split('/');
+        if (ids.Length > 3)
+        {
+            await RespondAsync($"The provided url `{messageUrl}` does not point to a Discord message");
+            return;
+        }
+
+        if (!ulong.TryParse(ids[0], out ulong guildId) && Context.Guild.Id != guildId)
+        {
+            await RespondAsync($"The provided url `{messageUrl}` needs to point to a message in this server");
+            return;
+        }
+
+        if (!ulong.TryParse(ids[1], out ulong channelId) && !Context.Guild.TextChannels.Any(x => x.Id == channelId))
+        {
+            await RespondAsync($"The provided url `{messageUrl}` does not point to a text channel that I can see");
+            return;
+        }
+
+        if (!ulong.TryParse(ids[2], out ulong msgId))
+        {
+            await RespondAsync($"The provided url `{messageUrl}` does not point to an existing message");
+            return;
+        }
+
+        var targetChannel = Context.Guild.GetTextChannel(channelId);
+        var targetMsg = await targetChannel.GetMessageAsync(msgId);
+        var targetImages = targetMsg.Attachments.Where(x => x.ContentType.StartsWith("image"));
+
+        if (targetImages.Count() == 0)
+        {
+            await RespondAsync("The provided message does not have an image attachment (I can't do urls yet)");
+            return;
+        }
+
+        var target = targetImages.ElementAtOrDefault(index);
+        if (target == null)
+        {
+            await RespondAsync($"There is no attachment at the specified index {index} on this message");
+            return;
+        }
+
+        using var tesseract = new TesseractEngine(@"./tessdata", "eng");
+        var bits = await _http.GetByteArrayAsync(target.Url);
+        var image = Pix.LoadFromMemory(bits);
+
+        using var page = tesseract.Process(image);
+        var resultText = page.GetText();
+
+        timer.Stop();
+        var embed = new EmbedBuilder()
+            .WithTitle($"OCR Result in {timer.ElapsedMilliseconds}ms with {page.GetMeanConfidence() * 100}% confidence")
+            .WithDescription(string.IsNullOrWhiteSpace(resultText) ? "*No Text Found*" : Format.Code(resultText))
+            .WithImageUrl(target.Url);
+
+        await RespondAsync(targetMsg.GetJumpUrl(), embeds: [ embed.Build() ]);
+    }
+}
